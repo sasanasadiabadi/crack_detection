b837edc97896b8e4fd5f94b2409dc0e53a352b25
iamsasan <36397328+sasanasadiabadi@users.noreply.github.com>
2018-05-18 10:01:48 +0300

    Update cnn_train.py

--- cnn_train.py
+++ cnn_train.py
@@ -7,17 +7,20 @@ import re
 
 from keras.models import Sequential
 from keras.layers import Conv2D, Dense, MaxPool2D, Flatten, Dropout
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.model_selection import train_test_split
 
 #################### set parameters ######################
+
+path = '../path/to/data'
 wx = 13
 wy = 2
 
 #################### create dataset ######################
-path = '../path/to/data'
 
 def create_data(path,wx,wy):
-    Xtrn = []
-    ytrn = []
+    X = []
+    y = []
     for roots, dirs, files in os.walk(path):
         #files.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
         convert = lambda text: int(text) if text.isdigit() else text
@@ -66,20 +69,20 @@ def create_data(path,wx,wy):
                     Xn.append(patch)
                     yn.append(label)
 
-        Xtrn = np.vstack((Xp,Xn))
-        ytrn = np.vstack((yp,yn))
+        X = np.vstack((Xp,Xn))
+        y = np.vstack((yp,yn))
 
-    return np.array(Xtrn), np.array(ytrn)
+    return np.array(X), np.array(y)
 
-Xtrn, ytrn = create_data(path,wx,wy)
+X, y = create_data(path,wx,wy)
 
-print(np.shape(Xtrn))
-print(np.shape(ytrn))
+print(np.shape(X))
+print(np.shape(y))
 
-Xtrn = Xtrn.astype(np.float32)
-ytrn = ytrn.astype(np.float32)
-Xtrn /= 255.0
-ytrn /= 255.0
+X = X.astype(np.float32)
+y = y.astype(np.float32)
+X /= 255.0
+y /= 255.0
 print("data normalized")
 
 ################### train CNN model ########################
@@ -106,16 +109,19 @@ def cnn_model():
 
     return model
 
+# train/validation split
+Xtrn, Xtst, ytrn, ytst = train_test_split(X,y,test_size=0.2,shuffle=True,random_state=42)
+
+early_stop = EarlyStopping(monitor='val_loss', patience=5, verbose=0, mode='auto')
+mdl_save = ModelCheckpoint('mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='auto')
+
+
 model = cnn_model()
-model.fit(Xtrn,ytrn,batch_size=128,epochs=10,verbose=2)
+model.fit(Xtrn,ytrn,batch_size=128,epochs=10,shuffle=True,verbose=2,
+          validation_data=(Xtst,ytst),callbacks=[early_stop,mdl_save])
 
+### save model
 file = os.path.expanduser('~') + '../path/to/directory/model.json'
-try:
-    os.remove(file)
-except OSError:
-    pass
-
-file = os.path.expanduser('~') + '../path/to/directory/weights.h5'
 try:
     os.remove(file)
 except OSError:
@@ -124,7 +130,5 @@ except OSError:
 model_json = model.to_json()
 with open("model.json", "w") as json_file:
     json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("weights.h5")
 
 print("Model saved to disk")
